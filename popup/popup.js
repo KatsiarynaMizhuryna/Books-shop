e321399f827bc1918e7d6acd87c97d44d2c3c7d0
Kate <ekaterina21051988@yandex.by>
2023-03-01 20:34:10 +0200

    fix: changed class for popup-header

--- popup/popup.js
+++ popup/popup.js
@@ -12,7 +12,7 @@ function CreatePopup(event) {
       let mod = event.target.parentElement.dataset.id;
       popupWrapper.innerHTML =`
 <div class ='popup-item'>
-       <h3 class = 'mpopup-header'>${booksCatalog[mod].title}</h3>
+       <h3 class = 'popup-header'>${booksCatalog[mod].title}</h3>
        <p class ='popup-text'>${booksCatalog[mod].description}</p>
        <button class="popup-close">&times<button>
 </div>`
