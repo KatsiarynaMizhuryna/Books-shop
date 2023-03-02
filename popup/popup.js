062b0063e9fa5c8ece665fb3c096d136d89191ad
Kate <ekaterina21051988@yandex.by>
2023-03-02 10:40:18 +0200

    feat: added function fo ropening and closing popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,37 @@
 const popupWrapper = document.createElement('div')
-      popupWrapper.classList.add('popup-wrapper')
+      popupWrapper.classList.add('popup-close')
       document.body.append(popupWrapper)
 
 window.addEventListener('click', function(event) {
     if (event.target.classList.contains('button-description')) {
         popupWrapper.classList.add('popup-open');
         CreatePopup(event);
+        event.preventDefault()
     }
 });
+//open popup
 function CreatePopup(event) {
-      let mod = event.target.parentElement.dataset.id;
+    let mod = event.target.parentElement.dataset.id;
       popupWrapper.innerHTML =`
-<div class ='popup-item'>
+    <div class ='popup-item'>
        <h3 class = 'popup-header'>${booksCatalog[mod].title}</h3>
        <p class ='popup-text'>${booksCatalog[mod].description}</p>
-       <button class="popup-close">&times<button>
-</div>`
+       <button class="popup-button">&times<button>
+    </div>`
+
+    let closeButton = document.querySelector('.popup-button')
+closeButton.addEventListener('click',closePopup)
 }
+window.addEventListener('click', (event) => {
+    if (event.target === popupWrapper) {
+      closePopup()
+    }
+  })
+
+//close popup
+function closePopup(event) {
+    popupWrapper.classList.remove('popup-open')
+    event.preventDefault()
+}
+
+
