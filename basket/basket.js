09f350ba7502eb2a256f18d63699b3b7ec3792a2
Kate <ekaterina21051988@yandex.by>
2023-03-08 08:42:03 +0200

    feat:added drop and drag

--- basket/basket.js
+++ basket/basket.js
@@ -19,7 +19,7 @@ basketBody.innerHTML = `
             <div class="basket-items">
             </div>
             <div class="basket-footer">
-            <a href="/delivery form/delivery.html" class="checkout-button">Checkout</a>
+            <a href="../delivery_form/delivery.html" class="checkout-button">Checkout</a>
             </div>
         </div>
     </div>
@@ -29,46 +29,54 @@ const basketTotal = document.querySelector('.basket-footer')
 //Add to basket
 window.addEventListener('click',function(event){
     if (event.target.classList.contains('button-add-to-basket')) {
-const card = event.target.closest('.book-card-item')
-const bookInfo = {
-        id: card.dataset.id,
-        imgSrc: card.querySelector('.book-image').getAttribute('src'),
-        author: card.querySelector('.book-autor').innerText,
-        title: card.querySelector('.book-title').innerText,
-        price: card.querySelector('.book-price').innerText
-        }
-//render items in basket
-const bookItemHTML = `
-<div class="basket-contains" data-id = "${bookInfo.id}">
-    <div><img class="add-image-to-basket" src="${bookInfo.imgSrc}" alt=" "></div>
-    <div class="basket-content">
-        <div>
-            <h3 class="add-name-to-basket">${bookInfo.author}</h3>
-            <p class="add-name-to-basket">${bookInfo.title}</p>
-        </div>
-        <div>
-            <p class="add-price-to-basket">${bookInfo.price}</p>
-        </div>
-        <div class="basket-counter">1</div>
-        <div><button class="remove-book">&times</button></div>
-        </div>
-</div>
- `
- //check the same goods
-const bookInBasket = basketItems.querySelector(`[data-id="${bookInfo.id}"]`)
-
-   if (bookInBasket) {
-    const counter = bookInBasket.querySelector('.basket-counter')
-    counter.innerText = parseInt(++counter.innerText)
-        }
-   else {
-      basketItems.insertAdjacentHTML("beforeend",bookItemHTML)
-         }
-         toggleBasketStatus()
-         calkBasketPrice()
-}
+        renderCards (event)
+    }
 event.preventDefault()
 })
+
+function renderCards(event) {
+    let data
+    if (event.type === 'click') {
+        data = event.target.closest('.book-card-item')
+    } else if (event.type === 'drop') {
+        data = document.querySelector('.book-dragging')
+    }
+    const bookInfo = {
+        id: data.dataset.id,
+        imgSrc: data.querySelector('.book-image').getAttribute('src'),
+        author: data.querySelector('.book-autor').innerText,
+        title: data.querySelector('.book-title').innerText,
+        price: data.querySelector('.book-price').innerText
+    }
+    //render items in basket
+    const bookItemHTML = `
+    <div class="basket-contains" data-id = "${bookInfo.id}">
+        <div><img class="add-image-to-basket" src="${bookInfo.imgSrc}" alt=" "></div>
+        <div class="basket-content">
+            <div>
+                <h3 class="add-name-to-basket">${bookInfo.author}</h3>
+                <p class="add-name-to-basket">${bookInfo.title}</p>
+            </div>
+            <div>
+                <p class="add-price-to-basket">${bookInfo.price}</p>
+            </div>
+            <div class="basket-counter">1</div>
+            <div><button class="remove-book">&times</button></div>
+            </div>
+    </div>
+     `
+     //check the same goods
+    const bookInBasket = basketItems.querySelector(`[data-id="${bookInfo.id}"]`)
+    if (bookInBasket) {
+        const counter = bookInBasket.querySelector('.basket-counter')
+        counter.innerText = parseInt(++counter.innerText)
+    }
+    else {
+        basketItems.insertAdjacentHTML("beforeend",bookItemHTML)
+    }
+    toggleBasketStatus()
+    calkBasketPrice()
+}
 
 //remove goods
 basketItems.addEventListener('click', function (event) {
@@ -77,7 +85,7 @@ basketItems.addEventListener('click', function (event) {
         toggleBasketStatus()
         calkBasketPrice()
     }
-        })
+})
 
 function toggleBasketStatus () {
     const basketItems = document.querySelector('.basket-items')
@@ -85,7 +93,9 @@ function toggleBasketStatus () {
     //const deliveryForm = document.querySelector('#order-form');
     if (basketItems.children.length > 0) {
         basketEmptyBadge.classList.add('checkout-button_active')
-    }else {basketEmptyBadge.classList.remove('checkout-button_active')}
+    }else {
+        basketEmptyBadge.classList.remove('checkout-button_active')
+    }
 }
 
 // adding sum
@@ -107,52 +117,4 @@ function calkBasketPrice() {
     });
     totalHeader.innerText = `Total: ${totalPrice} $`
     totalItemsHeader.innerText = `Your basket: (${totalAmount} items)`
-console.log(totalAmount)
 }
-
-
-
-   /* window.addEventListener('click', function(event) {
-        if (event.target.classList.contains('button-add-to-basket')) {
-            addToBasket(event);
-            event.preventDefault()
-        }
-    })
-
-let basketArray = []
-
-function addToBasket(event){
-    let htmlTxt = ''
-    let basketItem = event.target.parentElement.dataset.id
-    let basketContain = document.querySelector('.basket-items')
-
-    basketArray.push(booksCatalog[basketItem])
-
-    let totalPrice = basketArray.reduce((total, item) => total + item.price, 0)
-    let totalItems = basketArray.length
-
-    basketArray.forEach(item => {
-        htmlTxt += `
-            <div><img class="add-image-to-basket" src="${item.imageLink}" alt=" "></div>
-            <div>
-                <h3 class="add-name-to-basket">${item.author}</h3>
-                <p class="add-name-to-basket">${item.title}</p>
-            </div>
-            <div>
-                <p class="add-price-to-basket">${item.price}$</p>
-            </div>
-            <div><input type="number" class="add-quantity-to-basketbasket" id="quantity" min='1' max='100' value = '1' ></div>
-            <div><button class="remove-book">&times</button></div>`
-    })
-
-    let counter = basketContain.querySelector(${item.id})
-    console.log(counter)
-    
-
-    basketContain.innerHTML = htmlTxt
-    let basketTotal = document.querySelector('.basket-footer')
-    basketTotal.innerHTML = `
-        <div class="subtotal">
-            Subtotal (${totalItems} items): $${totalPrice}
-        </div>`
-}*/
---
+++ drop/drop.js
@@ -0,0 +1,34 @@
+window.addEventListener('dragstart', function(event) {
+    if (event.target.classList.contains('book-card-item')) {
+        let dragItem = event.target;
+        console.log(dragItem)
+        dragItem.classList.add('book-dragging');
+    }
+    if (event.target.classList.contains('book-image')) {
+        let dragItem = event.target.parentElement;
+        console.log(dragItem)
+        dragItem.classList.add('book-dragging');
+    }
+});
+
+window.addEventListener('dragend', function(event) {
+    if (event.target.classList.contains('book-card-item')) {
+        let dragItem = event.target;
+        dragItem.classList.remove('book-dragging');
+    }
+    if (event.target.classList.contains('book-image')) {
+        let dragItem = event.target.parentElement;
+        dragItem.classList.remove('book-dragging');
+    }
+});
+
+window.addEventListener('dragover', function(event) {
+        event.preventDefault();
+});
+
+window.addEventListener('drop', function(event) {
+    if (event.target.classList.contains('basket-wrapper') ||
+        event.target.parentElement.classList.contains('basket-items')) {
+        renderCards(event)
+    }
+});
